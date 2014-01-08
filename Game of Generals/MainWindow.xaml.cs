c87fde6cb3d3d3ee92a8d8f39e4054e36fb0a5c9
Kezo <alexander.kazen@gmail.com>
2014-01-08 15:02:22 +0100

    Added Player and Pieces classes

--- Game of Generals/MainWindow.xaml.cs
+++ Game of Generals/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,59 +14,64 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Game_of_Generals
-{
+namespace Game_of_Generals {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+
+        public MainWindow() {
             InitializeComponent();
             paintGrid();
         }
 
-        private void paintGrid()
-        {
-			for (int i = 9; i > 0; --i)
-			{
-				RowDefinition row = new RowDefinition();
-				row.Height = new GridLength(1, GridUnitType.Star);
-				pnlBoardGrid.RowDefinitions.Add(row);
-			}
-			for (int i = 8; i > 0; --i)
-			{
-				ColumnDefinition column = new ColumnDefinition();
-				column.Width = new GridLength(1, GridUnitType.Star);
-				pnlBoardGrid.ColumnDefinitions.Add(column);
-			}
-            for (int i = pnlBoardGrid.RowDefinitions.Count(); i >= 0; --i)
-			{
-				for (int j = pnlBoardGrid.ColumnDefinitions.Count(); j >= 0; --j)
-				{
-					Rectangle rect = new Rectangle();
-					rect.Fill = Brushes.Black;
-					pnlBoardGrid.Children.Add(rect);
-					Grid.SetColumn(rect, j);
-					Grid.SetRow(rect, i);
-					rect.MouseEnter += rect_MouseEnter;
-					rect.MouseLeave += rect_MouseLeave;
-				}
-			}
-                
+        private void paintGrid() {
+            for (int i = 9; i > 0; --i) {
+                RowDefinition row = new RowDefinition();
+                row.Height = new GridLength(1, GridUnitType.Star);
+                pnlBoardGrid.RowDefinitions.Add(row);
+            }
+            for (int i = 8; i > 0; --i) {
+                ColumnDefinition column = new ColumnDefinition();
+                column.Width = new GridLength(1, GridUnitType.Star);
+                pnlBoardGrid.ColumnDefinitions.Add(column);
+            }
+            for (int i = pnlBoardGrid.RowDefinitions.Count(); i >= 0; --i) {
+                for (int j = pnlBoardGrid.ColumnDefinitions.Count(); j >= 0; --j) {
+                    Rectangle rect = new Rectangle();
+                    rect.Fill = Brushes.Black;
+                    pnlBoardGrid.Children.Add(rect);
+                    Grid.SetColumn(rect, j);
+                    Grid.SetRow(rect, i);
+                    rect.MouseEnter += rect_MouseEnter;
+                    rect.MouseLeave += rect_MouseLeave;
+                }
+            }
+
         }
 
-        void rect_MouseLeave(object sender, MouseEventArgs e)
-        {
+        void rect_MouseLeave(object sender, MouseEventArgs e) {
             Rectangle rect = sender as Rectangle;
             rect.Fill = Brushes.Black;
         }
 
-        private void rect_MouseEnter(object sender, MouseEventArgs e)
-        {
+        private void rect_MouseEnter(object sender, MouseEventArgs e) {
             Rectangle rect = sender as Rectangle;
             rect.Fill = Brushes.Blue;
         }
+    }
+
+    public class Player {
+        public ObservableCollection<Piece> pieces = new ObservableCollection<Piece>();
+
+        public Player() {
+            for (int i = 0; i > 16; ++i) {
+                //populate list with pieces
+            }
+        }
+    }
+
+    public class Piece {
+        private int rank;
     }
 }
